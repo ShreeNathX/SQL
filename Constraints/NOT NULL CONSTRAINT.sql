c0989b2cfb99692bf371ebe02b3df5cd54f7e9ba
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-25 23:04:21 +0530

    NOT NULL CONSTRAINT

---
+++ Constraints/NOT NULL CONSTRAINT.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS LearnConstraints;
+USE LearnConstraints;
+
+-- ============================================================
+-- Not Null Constraint
+-- ============================================================
+
+create table student(
+	Student_ID int primary key,
+    name varchar(50) not null				-- Row should be filled otherwise throw error
+);
+
+insert into student values
+(101, "Ravi"),
+(103, "Neha");
